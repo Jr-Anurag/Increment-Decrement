b661a598d30247386b3a8b7c1c55bdc5a4f4a0cd
Jr-Anurag <anuragmishra14725@gmail.com>
2024-09-08 00:58:08 +0530

    Adding UI and tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[100vw] h-[100vh] bg-[#344151] flex justify-center items-center flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl" >
+         Increment & Decrement
+      </div>
+      <div className='bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151] '>
+        <button  className='border-r-2 text-center w-20 text-5xl border-[#bfbfbf]'>   -   </button>
+        <div > </div>
+        <button  className='border-l-2 text-center w-20 text-5xl border-[#bfbfbf] '>   +  </button>
+      </div>
+      <button  className='bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg '>Reset</button>
     </div>
   );
 }
